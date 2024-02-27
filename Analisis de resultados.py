5529c2e85b740fb4ce37b7373aeb992b0c4427d6
Dráckaro <luisalejandrorodriguezotero@gmail.com>
2024-02-26 23:15:01 -0500

    Detalles finales

--- Analisis de resultados.py
+++ Analisis de resultados.py
@@ -9,25 +9,26 @@ print("Introduzca el valor esperado para rotura de maquina")
 F=int(input())
 print("Introduzca el valor esperado para reparacion de maquina")
 G=int(input())
-print("Introduzca el numero de maquinas de reemplazo")
-M=int(input())
-print("Introduzca la cantidad de maquinas en el sistema")
-numero_maquinas=int(input())
-print("Tiempo esperado segun teoria: "  + str(Estimate(F,G,M)))
+print("Introduzca la cantidad de maquinas en el sistema (n)")
+n=int(input())
+print("Introduzca la cantidad de maquinas de reemplazo (m)")
+m=int(input())
 
-# Media de Simulaciones
-Eventos=Multiples_Simulaciones(lambda:
-                E(F),lambda:N(G,G/10),
-                numero_maquinas,M,10000)
+print("Tiempo esperado segun estimación para Modelo 2: "  + str(Estimate(F,G,m)))
+
+
+# Simulaciones en modelo 1
+Eventos=Multiples_Simulaciones(lambda:E(F),lambda:N(G,G/10),n,m,10000, 1)
 Media=sum(Eventos)/len(Eventos)
 Desvio=sum([abs(x-Media) for x in Eventos])/len(Eventos)
-print("Media de los resultados obtenidos en simulaciones del 2do modelo: "+str(Media))
+print("Media de los resultados obtenidos en simulaciones del 1er modelo: "+str(Media))
 print("Desviacion de los resultados obtenidos en simulaciones: "+str(Desvio))
 
 print()
 
-Eventos=Multiples_Simulaciones(lambda:E(F),lambda:N(G,G/10),numero_maquinas,M,10000,False)
+#Simulaciones en modelo 2
+Eventos=Multiples_Simulaciones(lambda:E(F),lambda:N(G,G/10),n,m,10000, 2)
 Media=sum(Eventos)/len(Eventos)
 Desvio=sum([abs(x-Media) for x in Eventos])/len(Eventos)
-print("Media de los resultados obtenidos en simulaciones del 1er modelo: "+str(Media))
-print("Desviacion de los resultados obtenidos en simulaciones: 5"+str(Desvio))
+print("Media de los resultados obtenidos en simulaciones del 2do modelo: "+str(Media))
+print("Desviacion de los resultados obtenidos en simulaciones: "+str(Desvio))
--- reparaciones.py
+++ reparaciones.py
@@ -1,73 +1,89 @@
 import heapq
-import scipy.stats as stats
 
-def reparar_maquinas(F, G, m, n):
-    maquinas_a_reparar =   0
+#Modelo 1 (Sin pérdida de memoria)
+def reparar_maquinas(F, G, n, m):
+    maquinas_a_reparar = 0
     roturas = []
     heapq.heapify(roturas)
 
-    for _ in range(m):
+    #Calcular el tiempo de rotura de las n máquinas
+    for _ in range(n):
         heapq.heappush(roturas, F())
 
     t = 0
-    disponible = n
-    tiempo_reparacion = G()
+    disponible = m
+    tiempo_reparacion = G() #Tiempo de reparación de la próxima máquina que se rompa
     tiempo_para_reparar = 0
 
+    #Inicio de la simulación
     while True:
-        tiempo_para_reparar = roturas[0]-t
+        tiempo_para_reparar = roturas[0]-t #Tiempo de la próxima que se romperá
+
+        #Reparar máquinas rotas si las hay
         while maquinas_a_reparar != 0 and tiempo_reparacion < tiempo_para_reparar:
             disponible += 1
             maquinas_a_reparar -= 1
             tiempo_para_reparar -= tiempo_reparacion
             tiempo_reparacion = G()
 
-        if maquinas_a_reparar !=   0 and tiempo_reparacion > tiempo_para_reparar:
+        if maquinas_a_reparar != 0 and tiempo_reparacion > tiempo_para_reparar:
             tiempo_reparacion -= tiempo_para_reparar
 
+        #Actualizar el tiempo
         t = heapq.heappop(roturas)
 
-        if disponible ==   0:
+        #Comprobar que haya máquinas de respuesto
+        if disponible == 0:
             break
 
+        #Romper la próxima máquina y calcular el tiempo de duración del respuesto
         disponible -=   1
         maquinas_a_reparar +=   1
         heapq.heappush(roturas, t + F())
 
     return t
 
-def reparar_maquinas_V2(F, G, m, n):
-    maquinas_a_reparar =   0
-    t = 0
-    disponible = n
-    tiempo_reparacion = G()
 
+
+#Modelo 2(Con Pérdida de memoria)
+def reparar_maquinas_V2(F, G, n, m):
+    maquinas_a_reparar = 0
+    t = 0
+    disponible = m
+    tiempo_reparacion = G() #Tiempo de reparación de la próxima máquina que se rompa
+
+    #Inicio de simulación
     while True:
-        tiempo_para_reparar = F()
-        t += tiempo_para_reparar
+        tiempo_para_reparar = F() #Tiempo en que se romperá la próxima máquina
+        t += tiempo_para_reparar #Actualizar el tiempo
+
+        #Reparar máquinas rotas si las hay
         while maquinas_a_reparar != 0 and tiempo_reparacion < tiempo_para_reparar:
             disponible += 1
             maquinas_a_reparar -= 1
             tiempo_para_reparar -= tiempo_reparacion
             tiempo_reparacion = G()
 
-        if maquinas_a_reparar !=   0 and tiempo_reparacion > tiempo_para_reparar:
+        if maquinas_a_reparar != 0 and tiempo_reparacion > tiempo_para_reparar:
             tiempo_reparacion -= tiempo_para_reparar
 
-        if disponible ==   0:
+        #Comprobar que haya máquinas de repuesto
+        if disponible == 0:
             break
 
-        disponible -=   1
-        maquinas_a_reparar +=   1
+        disponible -= 1
+        maquinas_a_reparar += 1
     return t
 
-def Multiples_Simulaciones(F, G, m, n, Rep, V2=True):
+
+#Función para desarrollar varias simulaciones con los mismos parámetros
+def Multiples_Simulaciones(F, G, n, m, Rep, Modelo):
 
     Resultados = []
 
     for _ in range(Rep):
-        if V2:
-            Resultados.append(reparar_maquinas_V2(F, G, m, n))
+        if Modelo == 2:
+            Resultados.append(reparar_maquinas_V2(F, G, n, m))
         else:
-            Resultados.append(reparar_maquinas(F, G, m, n))
+            Resultados.append(reparar_maquinas(F, G, n, m))
     return Resultados
